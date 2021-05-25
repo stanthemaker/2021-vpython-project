3bd9be3dc4a3a9fbbdd8640de0a4505c685058db
stanthemaker <stanwong178@gmail.com>
2021-05-26 00:08:14 +0800

    variables set

--- project.py
+++ project.py
@@ -1,4 +1,11 @@
 from numpy import *
 from vpython import *
-123
-456
+B_ground = 45*10**(-3)
+u0 = 4*pi*10**(-7)
+R_earth = 6371*10**3
+i_earth = 4*(2**0.5)*B_ground*R_earth/u0
+earth = sphere( pos = vec( 0,0,0) , 
+    radius = R_earth , texture={'file':textures.earth}) # earth_orbit['r']??
+
+def 
+
