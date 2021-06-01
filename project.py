dc958710385d6f13a8d0ff556cd81b10d50824f5
stanthemaker <stanwong178@gmail.com>
2021-06-02 01:26:34 +0800

    add B_by_solar_storm

--- project.py
+++ project.py
@@ -5,8 +5,11 @@ u0 = 4*pi*10**(-7)
 R_earth,R_earth_core = 6371*10**3, 1220 * 1E3
 # i_earth = 4*(2**0.5)*B_ground*R_earth/u0
 mu = 9.15*1E-4/(u0*R_earth**3)
-
+V_solar_storm = vec(0,0,0)
+Q_electric = -1.602 * 1E-19
 h , N , k= 1E-2 ,50, 1E6
+w = vec(0,0,0)
+c = 299792458 
 n=250
 # theta=array([2*pi*i/n for i in range(n)])
 # L_ds=2*pi*R_earth_core /n 
@@ -35,11 +38,16 @@ earth = sphere( pos = vec( 0,0,0) ,
 
 # def BiotSavart(ds_vec,vec_r):
 #     return u0 * i_earth * ds_vec.cross(vec_r.norm())/(vec_r.mag**2 * 4*pi)
-
+def B_by_solar_storm(p):
+    theta = acos (dot(V_solar_storm,p-w) / V_solar_storm.mag / (p-w).mag)
+    E = Q_electric * (1-V_solar_storm**2/c**2) * (p-w) / (4*pi*u0) / (1-V_solar_storm**2 * sin(theta)**2 / c**2 )**1.5 /(p-w)**3
+    B = V_solar_storm.cross(E/c**2)
 def mag_field_at_p(p):
 
     c = mu/(4*pi*(p.x**2.0+p.y**2.0+p.z**2.0)**1.5)
-    return c*(3*(dot(vec(0,mu,0),norm(p))) * norm(p) - vec(0,mu,0))
+    B_earth = c*(3*(dot(vec(0,mu,0),norm(p))) * norm(p) - vec(0,mu,0))
+
+    return B_earth
 
 
 for i in range(0, N):
